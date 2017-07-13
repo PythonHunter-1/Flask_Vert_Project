e2105f58cae9a1c0e962498e9b6e04ef997984cf
Admin <daniel.petersson727@mail.com>
2017-07-13 04:39:10 +0200

    db_action py file attached for db table create and input sample data

---
+++ db_actions.py
@@ -0,0 +1,24 @@
+from todoapp import app
+from models import db
+
+db.create_all()
+
+from models import Category
+
+work = Category(name=u'name')
+home = Category(name=u'home')
+
+db.session.add(work)
+db.session.add(home)
+db.session.commit()
+
+from models import Priority
+
+high = Priority(name=u'high', value=3)
+medium = Priority(name=u'medium', value=2)
+low = Priority(name=u'low', value=1)
+ 
+db.session.add(high)
+db.session.add(medium)
+db.session.add(low)
+db.session.commit()
