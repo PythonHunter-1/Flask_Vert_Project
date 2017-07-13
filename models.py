64dfdbc9116d8c82890f2c4e157c861b684c8db2
Admin <admin@Admins-Mac.local>
2017-07-13 03:49:20 +0200

    postgres database xml and model attached wit configuration setting

---
+++ models.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+# begin
+
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
+
+class Todo (db.Model):
+    __tablename__ = "todo"
+    id = db.Column('id', db.Integer, primary_key = True)
+    category_id = db.Column('category_id', db.Integer, db.ForeignKey('category.id'))
+    priority_id = db.Column('priority_id', db.Integer, db.ForeignKey('priority.id'))
+    description = db.Column('description', db.Unicode)
+    creation_date = db.Column('creation_date', db.Date)
+    is_done = db.Column('is_done', db.Boolean)
+
+    priority = db.relationship('Priority', foreign_keys=priority_id)
+    category = db.relationship('Category', foreign_keys=category_id)
+
+class Priority (db.Model):
+    __tablename__ = "priority"
+    id = db.Column('id', db.Integer, primary_key = True)
+    name = db.Column('name', db.Unicode)
+    value = db.Column('value', db.Integer)
+
+class Category (db.Model):
+    __tablename__ = "category"
+    id = db.Column('id', db.Integer, primary_key = True)
+    name = db.Column('name', db.Unicode)
+
+# end
--- todoapp.py
+++ todoapp.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://daniel:asdf@localhost/todoapp'
+
 from views import *
 
 if __name__ == '__main__':
